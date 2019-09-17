4fae69fc728fb4b0a8f63ffd5b8743db3579e734
albertshops <alistoresali@gmail.com>
2019-09-17 20:00:30 +0100

    Add WildRydes

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_1PwsC5XOG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1dsn18791q2g6688vrelkoir36', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://0pf1z76rc8.execute-api.eu-west-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
